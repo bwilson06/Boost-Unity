d0b37bba48ee870f3bf8ea0fbbbdfff638862132
Bradley Wilson <bluedirtbike6@gmail.com>
2020-10-02 14:34:47 -0500

    change audio for death, and win.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float thrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
     Rigidbody rigidBody;
     AudioSource audioSource;
+    public enum State { Alive, Dying, Transcending }
+
+    public State state = State.Alive;
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +38,34 @@ public class Rocket : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision collision) {
-        print("collided");
+        if (state != State.Alive){
+            return ;
+        }
+        switch (collision.gameObject.tag){
+            case "Friendly":
+                // do nothing
+                break;
+            case "Win":
+            audioSource.PlayOneShot(success);
+            state =  State.Transcending;
+            Invoke("LoadNextScene", 1f);
+                break;
+            default:
+            audioSource.Stop();
+            audioSource.PlayOneShot(death);
+            state = State.Dying;
+            Invoke("LoadFirstScene", .6f);
+                break;
+        }
+
+    }
+
+    void LoadNextScene () {
+        SceneManager.LoadScene(1);
+    }
+
+    void LoadFirstScene () {
+        SceneManager.LoadScene(0);
     }
 
     void Thrust () {
@@ -38,7 +73,7 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)){
             rigidBody.AddRelativeForce(Vector3.up * thrust);
             if (!audioSource.isPlaying){
-                audioSource.Play();
+                audioSource.PlayOneShot(mainEngine);
             }  
         }else{
                 audioSource.Stop();
@@ -48,7 +83,9 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (state == State.Alive){
         Thrust();
         Rotate();
+        }
     }
 }
