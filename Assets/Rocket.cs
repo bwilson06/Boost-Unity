3e721dfff2d987a849f23425203f223cb4e310b7
Bradley Wilson <bluedirtbike6@gmail.com>
2020-10-01 19:23:37 -0500

    edit rocket scale and material in prefab mode.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    void ProcessUpdate () {
+        if (Input.GetKey(KeyCode.Space)){
+            rigidBody.AddRelativeForce(Vector3.up);
+        }else if (Input.GetKey(KeyCode.A)){
+            print("Left Pressed");
+        }else if (Input.GetKey(KeyCode.D)){
+            print("Right pressed.");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessUpdate();
+    }
+}
